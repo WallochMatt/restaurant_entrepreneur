0d5b089d3070be207e99a6f88238c3c7bc04e989
WallochMatt <matthewrwalloch@gmail.com>
2022-09-12 14:24:26 -0500

    Reached MVP,need to work on user/error handling

--- factory.py
+++ factory.py
@@ -1,4 +1,6 @@
 #import the child classes(dishes)
+
+
 from pizza import Pizza
 from lobster import Lobster
 from salad import Salad
@@ -10,21 +12,17 @@ class OrderFactory:
 
     @staticmethod
     def create_order(choice):# Order static
-        if choice == "Pizza":
-            return Pizza()
+        try:
+            if choice == "Pizza":
+                return Pizza()
 
-        elif choice == "Lobster":
-            return Lobster()
+            elif choice == "Lobster":
+                return Lobster()
 
-        elif choice == "Salad":
-            return Salad()
+            elif choice == "Salad":
+                return Salad()
 
-        else:#likely going to remove
-            print("Sorry, that was not a valid option, please reselect")
-            OrderFactory.create_order()#self does not work, but this may not be proper
-            #or maybe call place_order again
-
-
-pass
-
-
+        except TypeError:
+            print("Please select a menu item")
+        
+           
